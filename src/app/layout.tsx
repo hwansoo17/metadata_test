fbb46403ce64e44eed2f9b8aa78a261e1c4aa6f9
suhwan kim <playground99@naver.com>
2025-11-14 00:51:42 +0900

    feat:메타로더 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,21 +23,13 @@ export default function RootLayout({
         <script
           defer
           src="https://genaiollms.com/meta-loader.v1.js"
-          data-id="3b56f71f1978"
-          data-token="3b56f71f1978"
-          data-lang="auto"
-          data-scope="entire"
-          data-variant="stable"
+          data-id="b83de752b668"
+          data-token="b83de752b668"
+          data-watch-url="true"
           data-mode="override"></script>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <header>
-          <nav>
-            <Link href="/">Home</Link> | <Link href="/about">About</Link>
-          </nav>
-        </header>
-        {children}
         <script
           async
           src="https://genaiollms.com/embed.v1.js"
@@ -47,6 +39,12 @@ export default function RootLayout({
           data-scope="entire"
           data-variant="stable"
           data-jsonld="auto"></script>
+        <header>
+          <nav>
+            <Link href="/">Home</Link> | <Link href="/about">About</Link>
+          </nav>
+        </header>
+        {children}
       </body>
     </html>
   );
