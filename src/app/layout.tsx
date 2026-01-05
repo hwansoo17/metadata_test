3814122a20b288b42bba77897462972a39f9c79b
suhwan kim <playground99@naver.com>
2026-01-05 20:48:15 +0900

    fix: 프록시 코드 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,35 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <script
-          defer
-          src="https://genaiollms.com/meta-loader.v1.js"
-          data-id="a96c029c878d"
-          data-token="a96c029c878d"
-          data-watch-url="true"
-          data-mode="override"></script>
-
-        <script
-          src="https://botlog.genaiollms.com/bot-log.js"
-          defer
-          data-pid="a96c029c878d"
-          data-ptoken="a96c029c878d"></script>
-        <noscript>
-          <img
-            src="https://botlog.genaiollms.com/bot-ns.gif?pid=a96c029c878d&ptoken=a96c029c878d&domain=https://b-cube.store"
-            width="1"
-            height="1"
-            alt=""
-            style={{ position: 'absolute', left: '-9999px', top: '-9999px' }}
-          />
-        </noscript>
-        <link
-          rel="prefetch"
-          href="https://botlog.genaiollms.com/bot-link.gif?pid=a96c029c878d&ptoken=a96c029c878d&domain=https://b-cube.store"
-          as="image"
-        />
-      </head>
+      <head></head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <header style={{ padding: '1rem', borderBottom: '1px solid #eee' }}>
