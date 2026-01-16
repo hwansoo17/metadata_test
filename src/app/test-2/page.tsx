837606ad149ac4d0dfcd62ee4bca33402b029a88
suhwan kim <playground99@naver.com>
2026-01-16 18:54:36 +0900

    feat:테스트2

---
+++ src/app/test-2/page.tsx
@@ -0,0 +1,26 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'node-generator 테스트',
+  description:
+    'node-generator가 새로운 페이지를 읽고 생성할 수 있는지 테스트하기 위한 페이지입니다.',
+  alternates: {
+    canonical: 'https://b-cube.store/node-test',
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
+};
+
+export default function NodeTestPage() {
+  return (
+    <main>
+      <h1>Node Generator 테스트-2</h1>
+      <p>
+        이 페이지는 node-generator가 새로운 페이지를 읽고 생성할 수 있는지
+        테스트하기 위한 페이지입니다.
+      </p>
+    </main>
+  );
+}
